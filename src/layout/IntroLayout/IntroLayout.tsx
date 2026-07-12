6e3f90f28f93f5d30e705bcccd7edf24ae94c14a
Gabriel Xavier <100116691+0nlySpirit@users.noreply.github.com>
2026-07-12 16:07:09 -0400

    [release] new v1.2

--- src/layout/IntroLayout/IntroLayout.tsx
+++ src/layout/IntroLayout/IntroLayout.tsx
@@ -4,6 +4,7 @@ import ImageButton from '../../components/ImageButton'
 
 import GITHUB_ICON from '../../assets/images/icons/github.png'
 import LINKEDIN_ICON from '../../assets/images/icons/linkedin.png'
+import ITCHIO_ICON from '../../assets/images/icons/itchio.svg'
 
 export default function IntroLayout() {
 
@@ -11,22 +12,38 @@ export default function IntroLayout() {
     return (
         <div className={styles.il_main}>
             
+            <div className={styles.more_info}>
+                <p>dotxavket@gmail.com</p>
+                <p>@dotxavierket</p>
+                <i>[v1.2]</i>
+            </div>
+
             <div className={styles.external_links_container}>
                 <ImageButton 
                     icon={GITHUB_ICON} 
                     css={{
                         width: "30px",
-                        transform: "rotateZ(10deg)"
+                        opacity: "0"
                     }}
                     on_click={() => window.open("https://github.com/G4brielXavier")}
-                />
+                    />
                 <ImageButton 
                     icon={LINKEDIN_ICON} 
                     css={{
                         width: "30px",
-                        transform: "rotateZ(-10deg)"
+                        opacity: "0"
                     }}
                     on_click={() => window.open("https://www.linkedin.com/in/gabriel-xavier-5b64153b0/")}
+                />
+                <ImageButton 
+                    icon={ITCHIO_ICON} 
+                    css={{
+                        width: "30px",
+                        opacity: "0",
+                        filter: "invert(1)"
+                    }}
+                    
+                    on_click={() => window.open("https://dotxav.itch.io")}
                 />
             </div>
 
